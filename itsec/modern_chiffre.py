09dbab73b5457258a8e4ed387f2773ab41b95867
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-16 12:00:49 +0100

    feat: finish one time pad chiffre

--- itsec/modern_chiffre.py
+++ itsec/modern_chiffre.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+
 def encrypt_one_time_pad_chiffre(plain_text: str, k: str) -> list[int]:
     """
     Encrypts a string using the one-time pad chiffre.
@@ -9,11 +10,15 @@ def encrypt_one_time_pad_chiffre(plain_text: str, k: str) -> list[int]:
     res = []
     for i, c in enumerate(plain):
         res.append(c ^ key[i])
-
     return res
 
-def decrypt_one_time_pad_chiffre(cipher: list[int], k: str) -> str:
+
+def decrypt_one_time_pad_chiffre(cipher: list[int], k: str) -> Tuple[str, list[int]]:
     """
     Decrypts a string using the one-time pad chiffre.
     """
-    return ""
+    key = [ord(c) for c in k]
+    res = []
+    for i, c in enumerate(cipher):
+        res.append(c ^ key[i])
+    return ("".join([chr(x) for x in res]), res)
